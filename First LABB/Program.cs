7d2dc46e7595a3f047d48c7b248edf35698070df
Ibrahim Rassoul <ao7105@mau.se>
2023-08-31 10:34:38 +0200

    Första försök

--- First LABB/Program.cs
+++ First LABB/Program.cs
@@ -7,7 +7,23 @@
             
             Console.WriteLine("Välkommen till hänga gubbe!");
             Console.WriteLine("Skriv en Bokstav nedan");
-            Console.ReadLine();
+            string guess1 = Console.ReadLine();
+            
+            string[] Bokstaver = new string[3];
+
+            Bokstaver[0] = "b";
+            Bokstaver[1] = "o";
+            Bokstaver[2] = "k";
+
+            //Array with all the letters
+
+            if (guess1 = "b")
+            {
+                Console.WriteLine("b");
+            }
+
+           
+            
         }
     }
 }
