ecb2231b9a0dd2e5c449ae7eb96cdeb622ab473b
Måns de la Rosa <ao8649@WCCZC210BYRR>
2023-08-31 10:00:02 +0200

    a

--- First LABB/Program.cs
+++ First LABB/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Let's make HÄNGA GUBBE!");
         }
     }
 }
